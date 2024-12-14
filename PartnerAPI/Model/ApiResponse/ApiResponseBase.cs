501be625820d2c1ddafebb54add364eb584350b0
RyanLIM_Laptop\RyanLIM <soongxian@gmail.com>
2024-12-15 00:03:20 +0800

    Modified Result in ApiResponse to be Required

--- PartnerAPI/Model/ApiResponse/ApiResponseBase.cs
+++ PartnerAPI/Model/ApiResponse/ApiResponseBase.cs
@@ -1,7 +1,10 @@
-﻿namespace PartnerAPI.Model.ApiResponse
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartnerAPI.Model.ApiResponse
 {
     public class ApiResponseBase
     {
+        [Required(ErrorMessage = "Result is required.")]
         public int Result { get; set; }
     }
 }
--- PartnerAPI/Model/ApiResponse/ApiResponseSuccess.cs
+++ PartnerAPI/Model/ApiResponse/ApiResponseSuccess.cs
@@ -4,11 +4,7 @@ namespace PartnerAPI.Model.ApiResponse
 {
     public class ApiResponseSuccess : ApiResponseBase
     {
-        [Required(ErrorMessage = "TotalAmount is required.")]
-        [Range(0, long.MaxValue, ErrorMessage = "TotalAmount must be a positive value.")]
         public long TotalAmount { get; set; }
-        [Required(ErrorMessage = "TotalDiscount is required.")]
-        [Range(0, long.MaxValue, ErrorMessage = "TotalDiscount must be a positive value.")]
         public long TotalDiscount { get; set; }
         public long FinalAmount { get; set; }
     }
